f1e8533b13d968798be5bc750a209b921a7da824
Choising <chltmdals115@gmail.com>
2021-01-24 17:21:34 +0900

    chapter 3, apply strategy pattern 3

---
+++ src/main/java/io/github/oraindrop/dao/AddStatement.java
@@ -0,0 +1,28 @@
+package io.github.oraindrop.dao;
+
+import io.github.oraindrop.domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AddStatement implements StatementStrategy {
+
+    private User user;
+
+    public AddStatement(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+        PreparedStatement ps = c.prepareStatement(
+                "insert into user (id, name, password) values (?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        return ps;
+    }
+}
--- src/main/java/io/github/oraindrop/dao/UserDao.java
+++ src/main/java/io/github/oraindrop/dao/UserDao.java
@@ -14,18 +14,7 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "insert into user (id, name, password) values (?,?,?)");
-
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        jdbcContextWithStatementStrategy(new AddStatement(user));
     }
 
     public User get(String id) throws SQLException, ClassNotFoundException {
