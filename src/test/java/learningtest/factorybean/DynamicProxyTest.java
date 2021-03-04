03c5e680380a95852fa545b9563d239c66808288
Choising <chltmdals115@gmail.com>
2021-03-04 23:46:56 +0900

    chapter 6, test pointcut class filter

--- src/test/java/learningtest/factorybean/DynamicProxyTest.java
+++ src/test/java/learningtest/factorybean/DynamicProxyTest.java
@@ -5,6 +5,8 @@ import learningtest.jdk.HelloTarget;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
@@ -12,6 +14,46 @@ import org.springframework.aop.support.NameMatchMethodPointcut;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DynamicProxyTest {
+
+    @Test
+    public void classNamePointcutAdvisor() {
+        NameMatchMethodPointcut classMethodPointcut = new NameMatchMethodPointcut() {
+            public ClassFilter getClassFilter() {
+                return new ClassFilter() {
+                    @Override
+                    public boolean matches(Class<?> clazz) {
+                        return clazz.getSimpleName().startsWith("HelloT");
+                    }
+                };
+            }
+        };
+
+        classMethodPointcut.setMappedName("sayH*");
+        checkAdviced(new HelloTarget(), classMethodPointcut, true);
+
+        class HelloWorld extends HelloTarget {};
+        checkAdviced(new HelloWorld(), classMethodPointcut, false);
+
+        class HelloToby extends HelloTarget {};
+        checkAdviced(new HelloToby(), classMethodPointcut, true);
+    }
+
+    private void checkAdviced(Object target, Pointcut pointcut, boolean adviced) {
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(target);
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UppercaseAdvice()));
+        Hello proxiedHello = (Hello) pfBean.getObject();
+
+        if (adviced) {
+            assertEquals(proxiedHello.sayHello("Toby"), "HELLO TOBY");
+            assertEquals(proxiedHello.sayHi("Toby"), "HI TOBY");
+            assertEquals(proxiedHello.sayThankYou("Toby"), "Thank You Toby");
+        } else {
+            assertEquals(proxiedHello.sayHello("Toby"), "Hello Toby");
+            assertEquals(proxiedHello.sayHi("Toby"), "Hi Toby");
+            assertEquals(proxiedHello.sayThankYou("Toby"), "Thank You Toby");
+        }
+    }
 
     @Test
     public void proxyFactoryBean() {
