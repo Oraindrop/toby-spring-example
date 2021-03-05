10bf1115531416ea0202c57464e27f108aae676f
Choising <chltmdals115@gmail.com>
2021-03-05 23:49:58 +0900

    chapter 6, apply pointcut class filter

---
+++ src/main/java/io/github/oraindrop/advice/NameMatchClassMethodPointcut.java
@@ -0,0 +1,25 @@
+package io.github.oraindrop.advice;
+
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+import org.springframework.util.PatternMatchUtils;
+
+public class NameMatchClassMethodPointcut extends NameMatchMethodPointcut {
+
+    public void setMappedClassName(String mappedClassName) {
+        this.setClassFilter(new SimpleClassFilter(mappedClassName));
+    }
+
+    static class SimpleClassFilter implements ClassFilter {
+        private String mappedName;
+
+        public SimpleClassFilter(String mappedName) {
+            this.mappedName = mappedName;
+        }
+
+        @Override
+        public boolean matches(Class<?> clazz) {
+            return PatternMatchUtils.simpleMatch(mappedName, clazz.getSimpleName());
+        }
+    }
+}
